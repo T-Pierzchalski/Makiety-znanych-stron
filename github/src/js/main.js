d2d40339e640818da1772efcb68b8d1d415f4212
TrueDie3 <t.pierzchalski0@gmail.com>
2023-04-06 16:02:46 +0200

    extended menu
    Responsive menu :)

--- github/src/js/main.js
+++ github/src/js/main.js
@@ -1,5 +1,7 @@
 const profileDivider = document.querySelector(".profile-divider");
 const divider = document.querySelector(".divider");
+const extendMenu = document.querySelector(".extend-menu");
+const nav = document.querySelector(".extended");
 profileDivider.addEventListener("click", () => {
 	const ul = document.querySelector(".profile-divide");
 	ul.style.cssText = "display:block";
@@ -9,8 +11,18 @@ profileDivider.addEventListener("click", () => {
 });
 divider.addEventListener("click", () => {
 	const ul = document.querySelector(".divider2");
+	const profile = document.querySelector(".photo");
 	ul.style.display = "block";
+	profile.style.cssText = "z-index:-2";
 	ul.addEventListener("mouseleave", () => {
 		ul.style.cssText = "display:none";
+
+		profile.style.cssText = "z-index:0";
+	});
+});
+extendMenu.addEventListener("click", () => {
+	nav.style.display = "flex";
+	nav.addEventListener("mouseleave", () => {
+		nav.style.display = "none";
 	});
 });
